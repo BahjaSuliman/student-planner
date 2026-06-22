575c193ddfe8dd91237d1038d2e512c01adbc174
bahja suliman <bahjasx.04@gmail.com>
2026-06-22 17:10:39 +0100

    Set up Spring Boot student planner project

---
+++ fullstack-student-planner/src/main/java/org/bahja/fullstackstudentplanner/FullstackStudentPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.bahja.fullstackstudentplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackStudentPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackStudentPlannerApplication.class, args);
+    }
+
+}
